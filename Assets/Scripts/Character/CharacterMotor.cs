9256967843f02b46ab3aed0baee5f7b552e61919
Kelly Weaver <kel.weaver@gmail.com>
2014-06-10 23:05:16 -0400

    Initial commit with initial game setup

---
+++ Assets/Scripts/Character/CharacterMotor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMotor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameController/PlayerController.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject SelectedPlayer;
+}
